20eceac9f71e949c05267bcd85bfc01db0e0fc3b
AICodo <6186860+AICodo@users.noreply.github.com>
2026-07-28 19:02:11 +0800

    Update vmaware.hpp

--- src/vmaware.hpp
+++ src/vmaware.hpp
@@ -12840,12 +12840,13 @@ public:
 			std::cout << "[DEBUG] 666 4 up function return not false" << "\n";
 		}
 
-        constexpr std::array<std::array<u8, 4>, 5> opcodes{ {
-            //{ 0x0F, 0x01, 0xD8, 0xC3 }, // VMRUN
+        constexpr std::array<std::array<u8, 4>, 6> opcodes{ {
+			{ 0x0F, 0x01, 0xC1, 0xC3 }, // VMCALL
+			{ 0x0F, 0x01, 0xD8, 0xC3 }, // VMRUN
             { 0x0F, 0x01, 0xDA, 0xC3 }, // VMLOAD
             { 0x0F, 0x01, 0xDB, 0xC3 }, // VMSAVE
-            //{ 0x0F, 0x01, 0xDD, 0xC3 }, // CLGI
-            //{ 0x0F, 0x01, 0xDF, 0xC3 }  // INVLPGA
+            { 0x0F, 0x01, 0xDD, 0xC3 }, // CLGI
+            { 0x0F, 0x01, 0xDF, 0xC3 }  // INVLPGA
         } };
 
 		
@@ -12857,18 +12858,21 @@ public:
             PVOID base_address = nullptr;
             SIZE_T region_size = 0x1000;
 			if(countop==0){
-					std::cout << "[DEBUG] 666 SVM_EXCEPTIONS 0 run  { 0x0F, 0x01, 0xD8, 0xC3 }, // VMRUN" << "\n";
+					std::cout << "[DEBUG] 666 SVM_EXCEPTIONS 0 run  { 0x0F, 0x01, 0xC1, 0xC3 }, // VMCALL" << "\n";
 			}
 			if(countop==1){
-					std::cout << "[DEBUG] 666 SVM_EXCEPTIONS 1 run  { 0x0F, 0x01, 0xDA, 0xC3 }, // VMLOAD"  << "\n";
+					std::cout << "[DEBUG] 666 SVM_EXCEPTIONS 0 run  { 0x0F, 0x01, 0xD8, 0xC3 }, // VMRUN" << "\n";
 			}
 			if(countop==2){
-					std::cout << "[DEBUG] 666 SVM_EXCEPTIONS 2 run  { 0x0F, 0x01, 0xDB, 0xC3 }, // VMSAVE" << "\n";
+					std::cout << "[DEBUG] 666 SVM_EXCEPTIONS 1 run  { 0x0F, 0x01, 0xDA, 0xC3 }, // VMLOAD"  << "\n";
 			}
 			if(countop==3){
-					std::cout << "[DEBUG] 666 SVM_EXCEPTIONS 3 run  { 0x0F, 0x01, 0xDD, 0xC3 }, // CLGI"  << "\n";
+					std::cout << "[DEBUG] 666 SVM_EXCEPTIONS 2 run  { 0x0F, 0x01, 0xDB, 0xC3 }, // VMSAVE" << "\n";
 			}
 			if(countop==4){
+					std::cout << "[DEBUG] 666 SVM_EXCEPTIONS 3 run  { 0x0F, 0x01, 0xDD, 0xC3 }, // CLGI"  << "\n";
+			}
+			if(countop==5){
 					std::cout << "[DEBUG] 666 SVM_EXCEPTIONS 4 run  { 0x0F, 0x01, 0xDF, 0xC3 }  // INVLPGA" << "\n";
 			}
 			countop++;
