28162e047e715a9b0339b84d43b283b1ce466c45
AICodo <6186860+AICodo@users.noreply.github.com>
2026-09-14 21:32:32 +0800

    Update vmaware.hpp

--- src/vmaware.hpp
+++ src/vmaware.hpp
@@ -14810,6 +14810,8 @@ public:
                         /* OVMF's memory bounds of the SEC and PEI execution phases */
                         if ((base_addr == 0x830000 && blob_len == 0xD0000) ||
                             (base_addr == 0x900000 && blob_len == 0xE80000)) {
+							std::cout << "[DEBUG] 666 MEASURED_BOOT: (base_addr == 0x830000 && blob_len == 0xD0000) =" << (int)(base_addr == 0x830000 && blob_len == 0xD0000) << "\n";
+               				std::cout << "[DEBUG] 666 MEASURED_BOOT: (base_addr == 0x900000 && blob_len == 0xE80000) =" << (int)(base_addr == 0x900000 && blob_len == 0xE80000) << "\n";
                             return true;
                         }
                     }
